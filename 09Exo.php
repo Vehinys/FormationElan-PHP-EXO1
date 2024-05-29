b3f8ce5114d96c3e986bdc6ad331e4bb2ddcc0e9
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-05-29 15:27:04 +0200

    MAJ

--- 09Exo.php
+++ 09Exo.php
@@ -4,15 +4,6 @@
    Si la personne est une femme dont l’âge est compris entre 18 et 35 ans ou si c’est un homme de plus de 20 ans,
    alors celle-ci est imposable (sinon ce n’est pas le cas, « non imposable »).</p>
 
-   // Si la femme est égale ou plus grand que 18 et qu'elle est égale ou plus petit que 35 ans, elle est imposable
-// Si l'homme est égale ou plus grand que 20 ans, il est imposable, 
-// Sinon non impossable
-<p>Créer une fonction personnalisée permettant 
-d’afficher des boutons radio avec un tableau de valeurs 
-en paramètre ("Monsieur","Madame","Mademoiselle").
-Exemple :
-afficherRadio($nomsRadio);</p>
-
 <H2>Le résultat</H2>
 
 <?php
