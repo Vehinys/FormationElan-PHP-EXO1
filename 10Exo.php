96b39ef9c3f0a3c6329ae22a7dfbb5cbf7b76c7f
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-05-31 10:49:46 +0200

    MAJ

--- 10Exo.php
+++ 10Exo.php
@@ -24,30 +24,30 @@ $compteurBilletCinq = 0;                       // Compteur pour les billets de 5
 $compteurPieceDeux  = 0;                       // Compteur pour les pièces de 2 €
 $compteurPieceUn    = 0;                       // Compteur pour les pièces de 1 €
 
-if ($reste >= 10) {
-    $compteurBilletDix = intdiv($reste, 10);   // Calcul du nombre de billets de 10 €
-    $reste %= 10;                              // Mise à jour du reste
+while ($reste >= 10) {
+    $compteurBilletDix++;                      // Calcul du nombre de billets de 10 €
+    $reste -= 10;                              // Mise à jour du reste
 }
 
-if ($reste >= 5) {
-    $compteurBilletCinq = intdiv($reste, 5);   // Calcul du nombre de billets de 5 €
-    $reste %= 5;                               // Mise à jour du reste
+if  ($reste >= 5) {
+    $compteurBilletCinq ++;                    // Calcul du nombre de billets de 5 €
+    $reste -= 5;                               // Mise à jour du reste
 }
 
-if ($reste >= 2) {
-    $compteurPieceDeux = intdiv($reste, 2);    // Calcul du nombre de pièces de 2 €
-    $reste %= 2;                               // Mise à jour du reste
+while ($reste >= 2) {
+    $compteurPieceDeux ++;                     // Calcul du nombre de pièces de 2 €
+    $reste -= 2;                               // Mise à jour du reste
 }
 
 if ($reste >= 1) {
-    $compteurPieceUn = $reste;                 // Calcul du nombre de pièces de 1 €
-    $reste = 0;                                // Mise à jour du reste
+    $compteurPieceUn ++;                       // Calcul du nombre de pièces de 2 € 
+                                    // Mise à jour du reste
 }
 
 echo "$compteurBilletDix Billets de 10 € - ";  // afficher la manière dont le rendu de monnaie doit être effectué, en utilisant des billets de 10 €, 5 €, et des pièces de 2 € et 1 €.
 echo "$compteurBilletCinq Billets de 5 € - ";  // afficher la manière dont le rendu de monnaie doit être effectué, en utilisant des billets de 10 €, 5 €, et des pièces de 2 € et 1 €.
 echo "$compteurPieceDeux Pièces de 2 € - ";    // afficher la manière dont le rendu de monnaie doit être effectué, en utilisant des billets de 10 €, 5 €, et des pièces de 2 € et 1 €.
-echo "$compteurPieceUn Pièces de 1 €. ";       //  afficher la manière dont le rendu de monnaie doit être effectué, en utilisant des billets de 10 €, 5 €, et des pièces de 2 € et 1 €.
+echo "$compteurPieceUn Pièces de 1 €. ";       // afficher la manière dont le rendu de monnaie doit être effectué, en utilisant des billets de 10 €, 5 €, et des pièces de 2 € et 1 €.
 
 
 
