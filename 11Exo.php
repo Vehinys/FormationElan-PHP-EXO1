ed09104f4352d5ac21d45fd0a835ab26824fcf0c
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-05-31 11:04:18 +0200

    MAJ

--- 11Exo.php
+++ 11Exo.php
@@ -7,18 +7,22 @@
 $text1 = "Il y a ";                                                            // texte 1
 $text2 = "marques de voitures dans le tableau :";                              // texte 2
 
-$voiture = array("Peugeot","Renault","BMW","Mercedes<br>");                    // Tableau avec des types de marque
-$count = count($voiture);                                                      // compte le nombre de marque (4)
+$voitures = array("Peugeot","Renault","BMW","Mercedes<br>");                    // Tableau avec des types de marque
+$count = count($voitures);                                                      // compte le nombre de marque (4)
 
 echo "$text1 $count $text2<br>";                                               // afficher texte 1 avec le nombre de marque + le texte 2
 
-echo "$voiture[0]<br> $voiture[1]<br> $voiture[2]<br> $voiture[3]<br>";        // afficher les differents marque de voiture 
-                                
+echo "$voitures[0]<br> $voitures[1]<br> $voitures[2]<br> $voitures[3]<br>";        // afficher les differents marque de voiture 
 
+Echo "*******************<br>";
 
-foreach ($voiture as $afficher) {                                              // afficher les differents marque de voiture par foreach
-  echo "$afficher <br>";
+echo $affMarque = implode("<br>", $voitures);                                   // afficher différament les differents marque de voiture   
+
+Echo "*******************<br>";
+
+foreach ($voitures as $voiture) {                                              // afficher les differents marque de voiture par foreach
+  echo "$voiture <br>";
 }
+Echo "*******************";
 
-echo $affMarque = implode("<br>", $voiture);                                  // afficher différament les differents marque de voiture
 
